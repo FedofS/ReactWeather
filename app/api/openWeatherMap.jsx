2188f37786c02a140e8462ba2c69a17a026b47d5
Sergey Fedof <7ff77ff@gmail.com>
2017-04-12 09:42:05 +0300

    Initial commit

---
+++ app/api/openWeatherMap.jsx
@@ -0,0 +1,23 @@
+var axios = require('axios');
+
+// const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=c4e735ea8bd7e7b6dc8368c752517b2d&units=imperial';
+
+const OPEN_WEATHER_MAP_URL = `http://api.openweathermap.org/data/2.5/weather?appid=fc98cd361d8a2e62704af72fc2c84448&units=metric`;
+
+//   fc98cd361d8a2e62704af72fc2c84448
+module.exports = {
+  getTemp: function (location) {
+    var encodedLocation = encodeURIComponent(location);
+    var requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`;
+
+    return axios.get(requestUrl).then(function (res) {
+      if (res.data.cod && res.data.message) {
+        throw new Error(res.data.message);
+      } else {
+        return res.data.main.temp;
+      }
+    }, function (res) {
+      throw new Error(res.data.message);
+    });
+  }
+}
