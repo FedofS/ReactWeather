48fc4bb3e448d1e00e402c9d482e62cfef0d345f
Sergey Fedof <7ff77ff@gmail.com>
2017-04-25 10:46:41 +0300

    Add nav search support

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,13 +1,18 @@
 var React = require('react');
 var {Link, IndexLink} = require('react-router');
 
-
 var Nav = React.createClass({
   onSearch: function (e) {
-    e.preventDefault();
-    alert('Not yet wired up!');
-  },
+      e.preventDefault();
 
+      var location = this.refs.search.value;
+      var encodedLocation = encodeURIComponent(location);
+
+      if (location.length > 0) {
+        this.refs.search.value = '';
+        window.location.hash = '#/?location=' + encodedLocation;
+      }
+  },
   render: function () {
     return (
       <div className="top-bar">
@@ -15,22 +20,21 @@ var Nav = React.createClass({
           <ul className="menu">
             <li className="menu-text">React Weather App</li>
             <li>
-              <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight:'bold'}}>Get Weather</IndexLink>
+              <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
             </li>
             <li>
-              <Link to="/about" activeClassName="active" activeStyle={{fontWeight:'bold'}}>About</Link>
+              <Link to="/about" activeClassName="active"  activeStyle={{fontWeight: 'bold'}}>About</Link>
             </li>
             <li>
-              <Link to="/examples" activeClassName="active" activeStyle={{fontWeight:'bold'}}>Examples</Link>
+              <Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
             </li>
           </ul>
         </div>
-
         <div className="top-bar-right">
           <form onSubmit={this.onSearch}>
             <ul className="menu">
               <li>
-                <input type="search" placeholder="Search weater by city"/>
+                <input type="search" placeholder="Search weather by city" ref="search"/>
               </li>
               <li>
                 <input type="submit" className="button" value="Get Weather"/>
@@ -39,7 +43,6 @@ var Nav = React.createClass({
           </form>
         </div>
       </div>
-
     );
   }
 });
